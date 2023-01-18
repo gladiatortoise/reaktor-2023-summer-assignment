43d2fc4e5747790e01a0a6f7d47502475396a213
gladiatortoise <ezhacks2014@gmail.com>
2023-01-18 13:54:43 +0200

    fix cors

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,16 @@
 import express from 'express'
 import { DroneListPoller } from './classes/DroneListPoller'
 import { PORT } from './constants'
+import cors from 'cors'
 
 const shortPoller = new DroneListPoller().start()
 const app = express()
+
+app.use(cors({
+    'allowedHeaders': ['Content-Type'],
+    'origin': '*',
+    'preflightContinue': true
+}));
 
 app.get('/drones', (req, res) => {
     req
