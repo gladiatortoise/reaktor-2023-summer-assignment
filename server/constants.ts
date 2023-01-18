9c9e5977fc39f825624f6d37eafcb5383207de55
gladiatortoise <ezhacks2014@gmail.com>
2023-01-18 16:43:52 +0200

    fix port number

--- server/constants.ts
+++ server/constants.ts
@@ -4,4 +4,4 @@ export const NO_FLIGHT_ZONE_RADIUS = 100000 // 100 meters
 export const NO_FLIGHT_ZONE_CENTER = { x: 250000, y: 250000 }
 export const DELETE_PERSON_AFTER = 1000 * 60 * 10 // 10 minutes
 export const DELETE_DRONE_AFTER = 1000 * 60 * 10 // 10 minutes
-export const PORT = 6090
+export const PORT = 6092
